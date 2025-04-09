446e6b72a8212b37e66e2ee2b1e576dd04ca34fd
HECTOR MAURICIO RODRIGUEZ SALAZAR <99525363+HectorMau501@users.noreply.github.com>
2025-04-08 20:58:35 -0600

    Add files via upload

--- web/flutter_service_worker.js
+++ web/flutter_service_worker.js
@@ -40,7 +40,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "4ff0f445cd226884dbc9aa2c46e64ac5
 "canvaskit/skwasm_st.wasm": "56c3973560dfcbf28ce47cebe40f3206",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "flutter.js": "76f08d47ff9f5715220992f993002504",
-"flutter_bootstrap.js": "6e9d79098fe2b22b7463585630b6f247",
+"flutter_bootstrap.js": "cb59c3aa3c3776c28c73f23e22217180",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
